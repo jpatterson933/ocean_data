1bd8e33e363873691c7626b9b96890f49c6fdc45
jpatterson-cYRYJA <jpatterson933@ucla.edu>
2023-08-22 00:16:56 -0700

    displaying simple data

--- client/src/pages/Location.jsx
+++ client/src/pages/Location.jsx
@@ -6,6 +6,7 @@ import { QUERY_LOCATION } from "../utils/queries";
 
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Table from 'react-bootstrap/Table';
 
 function Location() {
 
@@ -23,35 +24,55 @@ function Location() {
         if (!loading && data && data.locations) {
             console.log(data.locations)
 
+            function convertTime(timezone, timestamp) {
+                const dateTime = new Date(timestamp * 1000);
+                console.log(dateTime)
+                const offsetMilliseconds = timezone * 1000;
+                const timeInTimezone = new Date(dateTime.getTime() + offsetMilliseconds);
+                // Extract the components of the date and time
+                const hours = timeInTimezone.getUTCHours();
+                const minutes = String(timeInTimezone.getUTCMinutes()).padStart(2, '0');
+                const seconds = String(timeInTimezone.getUTCSeconds()).padStart(2, '0');
+                const day = timeInTimezone.getUTCDate();
+                const month = timeInTimezone.getUTCMonth() + 1;
+
+                // Format the time with the specified format
+                const formattedTime = `${month}/${day} at ${hours % 12 === 0 ? 12 : hours % 12}:${minutes}:${seconds}${hours >= 12 ? 'pm' : 'am'}`;
+
+                return formattedTime;
+            }
+
+            const { name, countryCode, latitude, longitude, timezone, sunrise, sunset, weatherData } = data.locations[0]
 
 
 
             return (
                 <>
-                    {data.locations.map(location => {
-                        const { name, countryCode, latitude, longitude, timezone, sunrise, sunset } = location;
-                        return (
-                            <>
-                                <Card style={{ width: "18rem" }}>
-                                    <Card.Title>{name}</Card.Title>
-                                    <ListGroup className="list-group-flush">
-                                        <ListGroup.Item action variant="info">Country Code</ListGroup.Item>
-                                        <ListGroup.Item action>{countryCode}</ListGroup.Item>
-                                        <ListGroup.Item action variant="info">Latitude</ListGroup.Item>
-                                        <ListGroup.Item action>{latitude}</ListGroup.Item>
-                                        <ListGroup.Item action variant="info">Longitude</ListGroup.Item>
-                                        <ListGroup.Item action>{longitude}</ListGroup.Item>
-                                        <ListGroup.Item action variant="info">Timezone</ListGroup.Item>
-                                        <ListGroup.Item action>{timezone}</ListGroup.Item>
-                                        <ListGroup.Item action variant="info">Sunrise</ListGroup.Item>
-                                        <ListGroup.Item action>{sunrise}</ListGroup.Item>
-                                        <ListGroup.Item action variant="info">Sunset</ListGroup.Item>
-                                        <ListGroup.Item action>{sunset}</ListGroup.Item>
-                                    </ListGroup>
-                                </Card>
-                            </>
-                        )
-                    })}
+                    <Card>
+                        <Card.Title>{name}</Card.Title>
+                        <Table striped bordered hover >
+                            <thead>
+                                <tr>
+                                    <th>Country Code</th>
+                                    <th>Latitude</th>
+                                    <th>Longitude</th>
+                                    <th>Time Zone</th>
+                                    <th>Sunrise</th>
+                                    <th>Sunset</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>{countryCode}</th>
+                                    <th>{latitude}</th>
+                                    <th>{longitude}</th>
+                                    <th>{timezone}</th>
+                                    <th>{convertTime(timezone, sunrise)}</th>
+                                    <th>{convertTime(timezone, sunset)}</th>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Card>
                 </>
             )
         }
